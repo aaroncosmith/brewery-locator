1e972c01c436884b875b0026593cd630fdf206da
Zach Barbre <zach.barbre@gmail.com>
2020-02-12 16:08:10 -0500

    add brewery city api function

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
 'use strict'
+
+const ipApiURL = 'https://ipvigilante.com/64.124.76.250'
+
+
+
+
+function getBreweryByCity(city){
+    const breweryApiURL = `https://api.openbrewerydb.org/breweries?by_city=${city}`;
+    let breweryNames = []
+    get(breweryApiURL).then(response => {
+        response.map(brewery => {
+            if (brewery['brewery_type'] === 'micro' || brewery['brewery_type'] === 'brewpub' || brewery['brewery_type'] === 'regional'){
+                breweryNames.push(brewery["name"]);
+            }
+        })
+    })
+    return breweryNames;
+}
+
+//function getCityByIP()
+
+console.log(getBreweryByCity('atlanta'));
