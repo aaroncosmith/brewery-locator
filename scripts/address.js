6f4247d29c43f52eb6f58b36eb05dda54bf76fe0
Aaron Smith <aaroncosmith@gmail.com>
2020-02-13 15:35:34 -0500

    working on js

---
+++ scripts/address.js
@@ -0,0 +1,49 @@
+// try and take items from an array and inject them on a 
+// clickable link that sits on the actual address
+addressArray = [
+    "4440 August Lane, Kansas City, KS, 66106",
+    "3432 Piedmont Road, Atlanta, GA, 30305",
+    "220 Gatewood Ave, Virgina Beach, VA"
+];
+
+emptySearchArray = [];
+
+function mapsAdressSearch() {
+
+};
+
+
+
+document.getElementById('address-link1').href = "www.google.com";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var finalLink, result;
+// var link = document.getElementById("address-link1")
+
+// result = "1";
+
+// function adjustLink() {
+//     switch(result) {
+//         case "1":
+//             finalLink = "google.com";
+//             break;
+//         case "2":
+//             finalLink = "amazon.com";
+//             break;
+//         default:
+//     }
+//     link.setAttribute("href", finalLink);
+// }
