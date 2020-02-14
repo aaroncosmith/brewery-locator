c8da4b66d9b680e0f2d938a01575d1c07f67790d
Aaron Smith <aaroncosmith@gmail.com>
2020-02-13 20:15:37 -0500

    worked on injecting data into our html

--- scripts/address.js
+++ scripts/address.js
@@ -1,49 +1,48 @@
 // try and take items from an array and inject them on a 
 // clickable link that sits on the actual address
-addressArray = [
-    "4440 August Lane, Kansas City, KS, 66106",
-    "3432 Piedmont Road, Atlanta, GA, 30305",
-    "220 Gatewood Ave, Virgina Beach, VA"
+const brewArray = [
+    {
+        title: "The Brewery",
+        brew: "SUPER MEGA DEATH",
+        address: "4440 August Lane, Kansas City, KS, 66106",
+        website: "https://www.google.com",
+    },
+    {
+        title: "1The Brewery",
+        brew: "1SUPER MEGA DEATH",
+        address: "14440 August Lane, Kansas City, KS, 66106",
+        website: "www.1yourmom.com",
+    },
 ];
 
 emptySearchArray = [];
 
-function mapsAdressSearch() {
+function mapsAdressSearch(results) {
+    const orderedList = document.getElementById('firstorder');
+    console.log("FUCK YEAH");
+    innerHTML = "";
+    results.forEach(function(eaResult) {
+        orderedList.innerHTML = orderedList.innerHTML + `
+        <li class="brew-list">
+            <div class="brew-title">
+                ${eaResult.title}
+            </div>
+            <div class="brew-type">
+                ${eaResult.brew}
+            </div>
+            <div class="address-link">
+                <a href="https://www.google.com/maps/dir/Current+Location/${eaResult.address}">${eaResult.address}</a>
+            </div>
+            <div class="website">
+                <a href="${eaResult.website}">${eaResult.website}</a>
+            </div>
+        </li>
+        `
+
+    }) 
+
+    
 
 };
 
-
-
-document.getElementById('address-link1').href = "www.google.com";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var finalLink, result;
-// var link = document.getElementById("address-link1")
-
-// result = "1";
-
-// function adjustLink() {
-//     switch(result) {
-//         case "1":
-//             finalLink = "google.com";
-//             break;
-//         case "2":
-//             finalLink = "amazon.com";
-//             break;
-//         default:
-//     }
-//     link.setAttribute("href", finalLink);
-// }
+mapsAdressSearch(brewArray);
