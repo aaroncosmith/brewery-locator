6c9810abd07682161f4b5212d1ee72b5f2c9af91
Bobby W Bryant <sirbob1999@gmail.com>
2020-02-14 11:23:23 -0500

    buttonwork

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,28 @@
 'use strict'
+// const getUserInputButton = document.querySelector("#brewbutton")
+// getUserInputButton.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     console.log('clicked');});
+
+// document.getElementById("brewbutton").onclick = function() {
+//     document.getElementById("wholelist").innerHTML = "HeyHey";
+// }
+
+// document.getElementById("brewbutton").onclick = function() {
+//     document.getElementById("firstcomp").innerHTML = "YUP";
+// }
+
+let a = document.getElementById('wholelist'); 
+let arr = ["brew1", "brew2", "brew3"];
+
+document.getElementById('brewbutton').onclick = display; 
+console.log(a.value);
 
 
 
-const getUserInputButton = document.querySelector("#brewbutton")
-getUserInputButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log('clicked');})
+function display(){ 
+    // console.log(arr); 
+
+arr.forEach(indivbrew => {
+    console.log(indivbrew); }
+)}
