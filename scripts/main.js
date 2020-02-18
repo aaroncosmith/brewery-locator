651c03e8a6f7dfba8f3c6acb6810763d0573cc1a
Bobby W Bryant <sirbob1999@gmail.com>
2020-02-18 14:19:02 -0500

    eventhand

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,7 @@
 //     console.log('clicked');});
 
 // document.getElementById("brewbutton").onclick = function() {
-//     document.getElementById("wholelist").innerHTML = "HeyHey";
-// }
+//     function("getBreweryByCity");
 
 // document.getElementById("brewbutton").onclick = function() {
 //     document.getElementById("firstcomp").innerHTML = "YUP";
@@ -15,18 +14,27 @@
 // let a = document.getElementById('wholelist'); 
 // let arr = ["brew1", "brew2", "brew3"];
 
-document.getElementById('brewbutton').onclick = display; 
-console.log(a.value);
+// document.getElementById('brewbutton').onclick = display; 
+// console.log(a.value);
 
 
 
-function display(){ 
+// function display()
 
-    injectHTML(brewArray);
-    };
+//     injectHTML();
+//     };
     // console.log(arr); 
 
 // brewArray.forEach(indivbrew => {
     
 //     console.log(indivbrew); }
 // )}
+
+const brewLocation = document.getElementById("brewField").value;
+const brewbutton = document.getElementById("brewbutton");
+
+brewbutton.addEventListener("click", function(e) {
+    const brewLocation = document.getElementById("brewField").value;
+    console.log(brewLocation);
+    getBreweryByCity(brewLocation); 
+});
