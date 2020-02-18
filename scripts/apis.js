334ed761a7643a3eb6c178b46afd240d5d93959d
Zach Barbre <zach.barbre@gmail.com>
2020-02-18 14:11:41 -0500

    Modified APIs to use city and state. Update inject to capitalized brewery type. Started readme

--- scripts/apis.js
+++ scripts/apis.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let userLoation = []
+let userLocation = [];
 
 function sortBreweries(distanceBetween, sortedDistance, breweriesArray){
     let sortedBreweries = [];
@@ -31,48 +31,21 @@ function breweriesToArray(breweries) {
             }
         }
     })
-    calculateDistance(breweriesArray, userLoation);
+    calculateDistance(breweriesArray, userLocation);
 }
 
-// function breweryLatToArray(breweries) {
-//     breweries.map(brewery => {
-//         if (brewery['brewery_type'] === 'micro' || brewery['brewery_type'] === 'brewpub' || brewery['brewery_type'] === 'regional'){
-//             if (brewery['latitude'] !== null && brewery['longitude'] !== null){
-//                 breweriesLat.push(Number(brewery['latitude']));
-//             }
-//         }
-//     });
-// }
-
-// function breweryLongToArray(breweries) {
-//     breweries.map(brewery => {
-//         if (brewery['brewery_type'] === 'micro' || brewery['brewery_type'] === 'brewpub' || brewery['brewery_type'] === 'regional'){
-//             if (brewery['latitude'] !== null && brewery['longitude'] !== null){
-//                 breweriesLong.push(Number(brewery['longitude']));
-//             }
-//         }
-//     });
-// }
-
-function getBreweryByCity(city){
-    const breweryApiURL = `https://api.openbrewerydb.org/breweries?by_city=${city}`;
+function getBreweryByCity(city, state){
+    const breweryApiURL = `https://api.openbrewerydb.org/breweries?by_city=${city}&${state}`;
     get(breweryApiURL).then(response => {
         breweriesToArray(response);
     });
 }
 
-
-// function getCityByIP(){
-//     const ipApiURL = `http://ip-api.com/json/`;
-//     get(ipApiURL).then(response => {
-//         getBreweryByCity(response['city']);
-//     });
-// }
-
 function getCityByLocation(lat, long){
     const geoParseApi = `https://geocode.xyz/${lat},${long}?json=1`
     get(geoParseApi).then(response => {
-        getBreweryByCity(response.city);
+        console.log(response['city'], response['osmtags']['is-in-state']);
+        getBreweryByCity(response['city'], response['osmtags']['is-in-state']);
     })
 }
 
@@ -85,7 +58,7 @@ function getUserLocation () {
     userPosition()
     .then((pos) => {
         console.log('It worked!', pos.coords)
-        userLoation = [pos.coords.latitude, pos.coords.longitude]
+        userLocation = [pos.coords.latitude, pos.coords.longitude]
         getCityByLocation(pos.coords.latitude, pos.coords.longitude);
     })
     .catch((err) => {
